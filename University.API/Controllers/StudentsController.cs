c8a7fe5d127e7f6ac925d399a26d30d95ef5a157
Rafet <rafetr@risk.az>
2022-12-06 14:44:06 +0400

    init

---
+++ University.API/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.DAL.DataContext;
+using University.DAL.Entities;
+
+namespace University.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public StudentsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var student = await _dbContext.Students.ToListAsync();
+            
+            return Ok(student);
+        }
+
+        [HttpGet("{id?}")]
+        public async Task<IActionResult> Get([FromRoute] int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _dbContext.Students.FindAsync(id);
+
+            if (student == null)
+                return NotFound("Bele telebe movcud deyil");
+
+            return Ok(student);
+        }
+    }
+}
