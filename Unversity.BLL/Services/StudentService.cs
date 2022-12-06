0a3246c2cb1413a1a98682acf82cfdaf7950443e
Rafet <rafetr@risk.az>
2022-12-06 17:25:27 +0400

    remove unused reference

--- Unversity.BLL/Services/StudentService.cs
+++ Unversity.BLL/Services/StudentService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using University.DAL.DataContext;
+﻿using University.DAL.DataContext;
 using University.DAL.Entities;
 using University.DAL.Repository;
 using Unversity.BLL.Services.Contracts;
